b64f85f579551d6fce9452a7382c72e52a1da80a
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-01-08 00:39:42 -0300

    Update fcd.py

--- fcd.py
+++ fcd.py
@@ -49,3 +49,143 @@ dados.head()
 
 
 """
+
+
+#Trazendo os últimos registros, com parâmetros
+dados.tail(2)
+
+"""
+output:
+
+checking_status	duration	credit_history	purpose	credit_amount	savings_status	employment	installment_commitment	personal_status	other_parties	...	property_magnitude	age	other_payment_plans	housing	existing_credits	job	num_dependents	own_telephone	foreign_worker	class
+998	<0	45	'existing paid'	radio/tv	1845	<100	1<=X<4	4	'male single'	none	...	'no known property'	23	none	'for free'	1	skilled	1	yes	yes	bad
+999	0<=X<200	45	'critical/other existing credit'	'used car'	4576	100<=X<500	unemployed	3	'male single'	none	...	car	27	none	own	1	skilled	1	none	yes	good
+
+"""
+
+
+#Filtrar por nome da coluna
+dados[["duration"]] 
+
+
+""" 
+output:
+
+duration
+0	6
+1	48
+2	12
+3	42
+4	24
+...	...
+995	12
+996	30
+997	12
+998	45
+999	45
+1000 rows × 1 columns
+
+"""
+
+
+#filtrar linhas por indice
+dados.loc[1:3]
+
+
+"""
+output:
+
+checking_status	duration	credit_history	purpose	credit_amount	savings_status	employment	installment_commitment	personal_status	other_parties	...	property_magnitude	age	other_payment_plans	housing	existing_credits	job	num_dependents	own_telephone	foreign_worker	class
+1	0<=X<200	48	'existing paid'	radio/tv	5951	<100	1<=X<4	2	'female div/dep/mar'	none	...	'real estate'	22	none	own	1	skilled	1	none	yes	bad
+2	'no checking'	12	'critical/other existing credit'	education	2096	<100	4<=X<7	2	'male single'	none	...	'real estate'	49	none	own	1	'unskilled resident'	2	none	yes	good
+3	<0	42	'existing paid'	furniture/equipment	7882	<100	4<=X<7	2	'male single'	guarantor	...	'life insurance'	45	none	'for free'	1	skilled	2	none	yes	good
+
+"""
+
+#linhas 1 e 3
+dados.loc[[1,3]]
+
+
+"""
+output:
+
+checking_status	duration	credit_history	purpose	credit_amount	savings_status	employment	installment_commitment	personal_status	other_parties	...	property_magnitude	age	other_payment_plans	housing	existing_credits	job	num_dependents	own_telephone	foreign_worker	class
+1	0<=X<200	48	'existing paid'	radio/tv	5951	<100	1<=X<4	2	'female div/dep/mar'	none	...	'real estate'	22	none	own	1	skilled	1	none	yes	bad
+3	<0	42	'existing paid'	furniture/equipment	7882	<100	4<=X<7	2	'male single'	guarantor	...	'life insurance'	45	none	'for free'	1	skilled	2	none	yes	good
+
+"""
+
+
+#filtro
+dados.loc[dados['purpose'] == "radio/tv"]
+
+
+"""
+output:
+
+	checking_status	duration	credit_history	purpose	credit_amount	savings_status	employment	installment_commitment	personal_status	other_parties	...	property_magnitude	age	other_payment_plans	housing	existing_credits	job	num_dependents	own_telephone	foreign_worker	class
+0	<0	6	'critical/other existing credit'	radio/tv	1169	'no known savings'	>=7	4	'male single'	none	...	'real estate'	67	none	own	2	skilled	1	yes	yes	good
+1	0<=X<200	48	'existing paid'	radio/tv	5951	<100	1<=X<4	2	'female div/dep/mar'	none	...	'real estate'	22	none	own	1	skilled	1	none	yes	bad
+8	'no checking'	12	'existing paid'	radio/tv	3059	>=1000	4<=X<7	2	'male div/sep'	none	...	'real estate'	61	none	own	1	'unskilled resident'	1	none	yes	good
+12	0<=X<200	12	'existing paid'	radio/tv	1567	<100	1<=X<4	1	'female div/dep/mar'	none	...	car	22	none	own	1	skilled	1	yes	yes	good
+15	<0	24	'existing paid'	radio/tv	1282	100<=X<500	1<=X<4	4	'female div/dep/mar'	none	...	car	32	none	own	1	'unskilled resident'	1	none	yes	bad
+...	...	...	...	...	...	...	...	...	...	...	...	...	...	...	...	...	...	...	...	...	...
+989	0<=X<200	24	'critical/other existing credit'	radio/tv	1743	<100	>=7	4	'male single'	none	...	'life insurance'	48	none	own	2	'unskilled resident'	1	none	yes	good
+991	'no checking'	15	'all paid'	radio/tv	1569	100<=X<500	>=7	4	'male single'	none	...	car	34	bank	own	1	'unskilled resident'	2	none	yes	good
+992	<0	18	'existing paid'	radio/tv	1936	'no known savings'	4<=X<7	2	'male mar/wid'	none	...	car	23	none	rent	2	'unskilled resident'	1	none	yes	good
+997	'no checking'	12	'existing paid'	radio/tv	804	<100	>=7	4	'male single'	none	...	car	38	none	own	1	skilled	1	none	yes	good
+998	<0	45	'existing paid'	radio/tv	1845	<100	1<=X<4	4	'male single'	none	...	'no known property'	23	none	'for free'	1	skilled	1	yes	yes	bad
+
+
+"""
+
+
+#outra condição
+dados.loc[dados['credit_amount'] >  18000]
+
+"""
+output:
+
+
+checking_status	duration	credit_history	purpose	credit_amount	savings_status	employment	installment_commitment	personal_status	other_parties	...	property_magnitude	age	other_payment_plans	housing	existing_credits	job	num_dependents	own_telephone	foreign_worker	class
+915	0<=X<200	48	'no credits/all paid'	other	18424	<100	1<=X<4	1	'female div/dep/mar'	none	...	'life insurance'	32	bank	own	1	'high qualif/self emp/mgmt'	1	yes	no	bad
+
+"""
+
+
+#atribuimos resultado a variável, criando outro df
+credito2 = dados.loc[dados['credit_amount'] >  18000]
+print(credito2)
+
+
+"""
+output:
+
+    checking_status  duration         credit_history purpose  credit_amount  \
+915        0<=X<200        48  'no credits/all paid'   other          18424   
+
+    savings_status employment  installment_commitment       personal_status  \
+915           <100     1<=X<4                       1  'female div/dep/mar'   
+
+    other_parties  ...  property_magnitude age  other_payment_plans housing  \
+915          none  ...    'life insurance'  32                 bank     own   
+
+    existing_credits                          job num_dependents  \
+915                1  'high qualif/self emp/mgmt'              1   
+
+     own_telephone foreign_worker class  
+915            yes             no   bad  
+
+[1 rows x 21 columns]
+"""
+
+#definimos só algumas colunas
+credito3 = dados[['checking_status','duration']].loc[dados['credit_amount'] >  18000]
+print(credito3)
+
+"""
+    checking_status  duration
+915        0<=X<200        48
+
+"""
+
